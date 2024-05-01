e2aa3c447f01973692151248f737cb543c11f61f
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-05-01 12:44:13 +0200

    txt to excel structure update
    wrap in functions

--- src/transformd_text_to_excel.py
+++ src/transformd_text_to_excel.py
@@ -10,34 +10,78 @@ def read_text(path_text:str) -> str:
         return f.read()
 
 
+def prune_doc(doc:str) -> str:
+    # replace the whole line of "NEXT QUESTION IS ..." by start flag
+    pattern = "^NEXT.*$"
+    doc = re.sub(pattern, "start_flag", doc, flags=re.MULTILINE)
+    # i no longer need "xx min xx seconds"
+    pattern = "^.*min.*seconds$"
+    doc = re.sub(pattern, "", doc, flags=re.MULTILINE | re.IGNORECASE)
+    # Delete "most voted"
+    pattern = "most voted"
+    doc = re.sub(pattern, "", doc, flags=re.IGNORECASE)   
+    return doc
 
-def prune_text(str_doc:str):
+
+
+def retrieve_label(doc:str) -> str:
+    # match the question label
+    # From start flag until r"A.", it can contain any character including new line
+    pattern = "start_flag(?:\n|\r|.)*A\."
+    match = re.search(pattern, doc, re.MULTILINE)
+    if match:
+        str_label = match[0]
+        # slice the heading "start_flag" and the tailing "A."
+        str_label = str_label[len("start_flag") : -2]
+        # replace newline by space
+        str_label = str_label.replace("\n", " ").replace("\r", " ").strip()
+        # sometimes there are "\n+ A.", now you can find them by " +"
+        str_label = str_label.replace(" +", "").strip()
+        return str_label
+    else:
+        print("label not found")
+        return "error"
+
+
+def retrieving(str_doc:str):
     list_doc = str_doc.split("%>%")
     list_label = []
+    list_answers = []
+
+    list_choice_1 = [] # A
+    list_flag_1 = []
+
+    list_choice_2 = [] # B
+    list_flag_2 = []
+
+    list_choice_3 = [] # C
+    list_flag_3 = []
+
+    list_choice_4 = [] # D
+    list_flag_4 = []
+
+    list_choice_5 = [] # E
+    list_flag_5 = []
+
+    list_choice_6 = [] # F
+    list_flag_6 = []
+
+    list_choice_7 = [] # G
+    list_flag_7 = []
+
+    list_choice_8 = [] # H
+    list_flag_8 = []
+
     for doc in list_doc:
-        # replace "NEXT QUESTION IS" by start flag
-        pattern = "^NEXT.*$"
-        doc = re.sub(pattern, "start_flag", doc, flags=re.MULTILINE)
+        
 
-        # match the question label
-        # From start flag until r"A.", it can contain any character including new line
-        pattern = "start_flag(?:\n|\r|.)*A\."
-        match = re.search(pattern, doc, re.MULTILINE)
-        if match:
-            str_label = match[0]
-            # slice the heading "start_flag" and the tailing "A."
-            str_label = str_label[len("start_flag") : -2]
-            # replace newline by space
-            str_label = str_label.replace("\n", " ").replace("\r", " ").strip()
-            # sometimes there are "\n+ A.", now you can find them by " +"
-            str_label = str_label.replace(" +", "").strip()
-            list_label.append(str_label)
-        else:
-            print("label not found")
-            list_label.append("error")
-            breakpoint()
+        
 
-        # end flag will be "correct answer is"
+        # Retrieve label
+        list_label.append(retrieve_label(doc))
+
+        # Retrieve each choice. We assume that there are at least A and B
+
 
     breakpoint()
 
@@ -45,7 +89,7 @@ def prune_text(str_doc:str):
 def main():
     path_text = "../out/doc_cad0_truncated.txt"
     str_doc = read_text(path_text)
-    prune_text(str_doc)
+    retrieving(str_doc)
 
 if __name__ == "__main__":
     main()
